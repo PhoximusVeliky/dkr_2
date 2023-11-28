27b329729f81da6bdc307dc4038fcfdedcc96d44
PhoximusVeliky <144343315+PhoximusVeliky@users.noreply.github.com>
2023-11-28 17:04:33 +0300

    Add files via upload

---
+++ dkr_2/z21.pas
@@ -0,0 +1,20 @@
+﻿program z21;
+
+var i,min,max,n:integer;
+A:array [0..100] of integer;
+
+  begin
+    min:=10231;
+    max:=-123512;
+    writeln('введите длину массива');
+    read(n);
+    writeln('введите массив (',n,' элементов)');
+    for i:=0 to n-1 do 
+    begin
+     readln(A[i]);
+     if a[i]<min then min:=a[i];                
+     if a[i]>max then max:=a[i];                    
+    end;
+    writeln('min=',min);
+    writeln('max=',max);
+  end.
---
+++ dkr_2/z22(good job).pas
@@ -0,0 +1,29 @@
+﻿Program z21;
+var i,n,k,j,ans:integer;
+    s:string;
+    A:array [1..765,1..3] of string;
+  begin
+
+   writeln('введите строку');
+   readln(s);
+   //ans:=0;
+   n:=length(s);
+   
+   for i:=1 to n-2 do 
+     for j:=1 to 3 do A[i][j]:=s[i-1+j];
+   //writeln(a);
+   
+   for i:=1 to n-2 do
+   begin
+     k:=0;
+     for j:=1 to n-2 do 
+     begin
+       if A[i]=A[j] then k+=1;
+       //writeln(a[i],'|',a[j],k,' ',ans,'|',i,'  ',j);
+     end;
+     if k=1 then ans+=1;
+   end;    
+   writeln(ans);
+  end.
+  //abvabv
+  //abv bva vab abv 
